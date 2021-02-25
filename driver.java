7990fc8373c8ba1abdeb5e66ab571867a1deb7e5
ashbyjoseph <55994855+ashbyjoseph@users.noreply.github.com>
2021-02-24 23:28:30 -0600

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,44 @@
+//import java.util.ArrayList;
+import java.util.HashSet;
+
+public class driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This was done completely on my own and a lookup of how to declare a ArrayList of <Characters>
+        String s1="";
+        String s2="AAAA";
+        String s3="ABCDv";
+        String s4="abbbbbbbbbb";
+        boolean value;
+         value=checkUnique(s4);
+         if(value==true) {
+        	 System.out.println("The string is unique");
+         }else {
+        	 System.out.println("The string contains duplicate chars");
+         }
+	}
+
+	
+	
+	public static boolean checkUnique(String string) {
+		string=string.toLowerCase();
+		if(string.isEmpty()) {
+			return true;
+		}
+		
+		HashSet<Character>chars = new HashSet<Character>(); // Using a hashset is much better for this coding problem
+		for(int c=0;c<string.length();c++) {
+			if(chars.contains(string.charAt(c))) { 
+				return false;
+			}else {
+				chars.add(string.charAt(c));
+			}
+			
+		}
+		
+		
+		
+		return true;
+	}
+}
